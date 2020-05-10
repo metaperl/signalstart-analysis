03eededc6b72c67a9b192ef9f6302302d54e88a4
Terrence Brannon <metaperl@gmail.com>
2020-05-10 05:42:01 -0400

    It now parses the details and main page properly.

---
+++ etc/clickalink.md
@@ -0,0 +1,13 @@
+I am trying to parse the data table at [this](https://www.signalstart.com/search-signals) page. 
+
+The second column in the table, `Name`, has a link in it with further data on that row.
+
+I want to click on that link and add more data to `data_row`, a dictionary where each key points to the value for a `<td>` cell in that row.
+
+My [code for iterating over the rows of the data table](https://github.com/metaperl/signalstart-analysis/blob/master/src/spiders/signalstart_spider.py#L34) works just fine. 
+
+But I need to enhance it to click the link, parse some fields on the details page for that data row and then go back to the previous page, `yield` the `data_row` and then continue iteration with the next row.
+
+# Answered 
+https://stackoverflow.com/questions/61705231/how-to-click-a-link-in-scrapy-parse-some-data-on-the-new-page-and-then-return-t
+
--- src/spiders/signalstart_spider.py
+++ src/spiders/signalstart_spider.py
@@ -10,6 +10,37 @@ class SignalStartSpider(scrapy.Spider):
         'https://www.signalstart.com/search-signals',
     ]
 
+    def parse_details(self, response):
+
+        def split_field():
+            pass
+
+        class Details(scrapy.Item):
+            xpath = scrapy.Field()
+            extractor = scrapy.Field()
+
+        fields = {
+            'won': Details(),
+            'profit_factor': Details(),
+            'daily': Details(),
+            'monthly': Details()
+        }
+
+        fields['won']['xpath'] = "//li[contains(text(),'Won:')]"
+        fields['won']['extractor'] = split_field
+
+        fields['profit_factor']['xpath'] = "//li[@class='list-group-item popovers']"
+
+        fields['daily']['xpath'] = "//li[contains(text(),'Daily:')]"
+        fields['monthly']['xpath'] = "//li[contains(text(),'Monthly:')]"
+
+        for field, field_processor in fields.items():
+            print(f"     Process {field}")
+            elem = response.xpath(field_processor['xpath'])
+            _, value = html_text.extract_text(elem.get()).split(':')
+            response.meta["data_row"][field] = value
+        yield response.meta["data_row"]
+
     def parse(self, response):
 
         cols = "rank name gain pips drawdown trades type monthly chart price age added action"
@@ -34,8 +65,12 @@ class SignalStartSpider(scrapy.Spider):
         for provider in response.xpath("//div[@class='row']//tr"):
             data_row = dict()
             Behold().show('provider')
+            details_url = None
+
             for i, datum in enumerate(provider.xpath('td')):
                 Behold().show('i', 'datum')
+                if i == 1: # name
+                    details_url = datum.css("a::attr(href)").get()
                 if i in skip:
                     print(".....skipping")
                     continue
@@ -44,8 +79,9 @@ class SignalStartSpider(scrapy.Spider):
                 if column_name in postprocess:
                     text = postprocess[column_name](text)
                 data_row[column_name] = text
-            yield data_row
+            if details_url:
+                yield scrapy.Request(url=details_url, callback=self.parse_details, meta={'data_row': data_row})
 
-        # next_page = response.css('.fa-angle-right').get()
+    # next_page = response.css('.fa-angle-right').get()
         # if next_page is not None:
         #     yield response.follow(next_page, self.parse)
