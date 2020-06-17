f2807fde4a1c671970f6cd19058c65338bfaec60
Sunshine Sexy Love <spamtrap@ayay42452345235.com>
2020-06-17 07:09:35 -0400

    scrape run Wed, Jun 17, 2020  7:09:35 AM

--- signalstart/signalstart/spiders/signalstart_spider.py
+++ signalstart/signalstart/spiders/signalstart_spider.py
@@ -69,7 +69,7 @@ class SignalStartSpider(scrapy.Spider):
 
         fields['won']['xpath'] = "//li[contains(text(),'Won:')]"
         fields['profit_factor']['xpath'] = "//li[@class='list-group-item popovers']"
-        fields['daily']['xpath'] = "//li[contains(text(),'Daily:')]"
+        fields['daily']['xpath'] = "//body//div[@class='row']//div[@class='row']//div[3]//ul[1]//li[2]"
         fields['monthly']['xpath'] = "//li[contains(text(),'Monthly:')]"
 
         logger.debug("--------------------------------------- Details parse")
